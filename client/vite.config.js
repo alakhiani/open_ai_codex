68c33efb4f737b545d67ea201a89504f905a001e
Avinash Lakhiani <alakhiani@yahoo.com>
2023-05-23 19:38:15 -0700

    Another try at getting the backend url.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig(() => {
     return {
         define: {
             __APP_ENV__: process.env.VITE_VERCEL_ENV,
-            __BACKEND_URL__: process.env.VITE_BACKEND_URL,
+            __BACKEND_URL__: process.env.BACKEND_URL,
         },
     };
 });
