3fa42a6e94b7f06c7a68fc5dbe7355debe583734
Avinash Lakhiani <alakhiani@yahoo.com>
2023-05-23 21:28:38 -0700

    Try again

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     define: {
         'process.env': {
-            __LOCAL_BACKEND_URL__: "http://localhost:5174",
-            __BACKEND_URL__: JSON.stringify(process.env.VITE_BACKEND_URL),
+            '__LOCAL_BACKEND_URL__': "http://localhost:5174",
+            '__BACKEND_URL__': process.env.VITE_BACKEND_URL,
         }
     }
 });
