2f41c6eac769494e4921cc649b19b97f94a5ab6f
Avinash Lakhiani <alakhiani@yahoo.com>
2023-05-23 21:30:17 -0700

    Trying NEXT_PUBLIC prefix instead of VITE

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
     define: {
         'process.env': {
             '__LOCAL_BACKEND_URL__': "http://localhost:5174",
-            '__BACKEND_URL__': process.env.VITE_BACKEND_URL,
+            '__BACKEND_URL__': process.env.NEXT_PUBLIC_BACKEND_URL,
         }
     }
 });
