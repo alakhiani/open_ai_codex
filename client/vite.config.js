9ed1324096de074b51633a9874c513cf801050af
Avinash Lakhiani <alakhiani@yahoo.com>
2023-05-23 21:33:51 -0700

    Try import meta env instead

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
     define: {
         'process.env': {
             '__LOCAL_BACKEND_URL__': "http://localhost:5174",
-            '__BACKEND_URL__': process.env.NEXT_PUBLIC_BACKEND_URL,
+            '__BACKEND_URL__': import.meta.env ? import.meta.env.BACKEND_URL : null,
         }
     }
 });
